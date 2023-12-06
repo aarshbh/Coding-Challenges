f2a499153219a9d105357687cdfea9ef5b96c65f
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-06 22:37:56 +0530

    Add files via upload

---
+++ Day-06/js.js
@@ -0,0 +1,14 @@
+let firstName = "mathew";
+firstName = "john";
+console.log(firstName);
+
+let favFood = "Pizza";
+let favColor = "Red";
+let favDrink = "Lemonade";
+
+console.log(favDrink);
+favfood = "Pasta";
+console.log(favFood);
+
+
+let birthDate="20";
