798392508988f03f675122827f31934c32e1f19f
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-06 22:37:41 +0530

    Add files via upload

---
+++ Day-05/05.js
@@ -0,0 +1,8 @@
+// prompt("What is your name?","Name");
+// console.log(confirm("Continue learnng JS"));
+
+console.log("Hey there I am been locked to the console");
+console.warn("This is a warning");
+console.error("This is an error");
+
+alertabc("This is also error");
