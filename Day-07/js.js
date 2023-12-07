9976060e9dc12aad974dc50ab189ef7787642afa
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-07 22:46:16 +0530

    Add files via upload

---
+++ Day-07/js.js
@@ -0,0 +1,36 @@
+let num = 7;
+let newNumber = 10;
+ num=20;
+ let favFood="pizza";
+ let favColor='yellow';
+
+ console.log(100>5);
+
+ let highNumber=462874628476;
+ console.log(typeof highNumber);
+
+ let myList=["Aarsh",true,"AArsh Bharti",9];
+ console.log(myList);
+ console.log(typeof myList);
+console.log(myList[0]);
+
+function hello(){
+    console.log("Hello World");
+
+}
+hello();
+
+let welcome=function(){
+    console.log("Hello World Again");
+}
+welcome();
+let character={
+
+    name:"Sonic Boom",
+    color:"Blue",
+    speed: 1000,
+    fast: true,
+
+}
+console.log(character.speed);
+
