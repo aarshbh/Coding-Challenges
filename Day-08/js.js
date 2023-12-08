70852384001263f370cc570d12b5d5c31dbb3fd4
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-08 22:42:47 +0530

    Add files via upload

---
+++ Day-08/js.js
@@ -0,0 +1,24 @@
+console.log(5+5);
+console.log(19-6);
+console.log(12*2);
+console.log(12/2);
+console.log(5%2);
+console.log(5**6); // 5 raised to power 6
+
+let score=99;
+console.log(++score);
+console.log(score);
+console.log(--score);
+
+
+let sum=2*5-7+11-(7-1);
+console.log(sum);
+
+let num1=10;
+let num2=7;
+console.log(num1+num2);
+console.log(num1%num2);
+
+var total=10;
+total++;
+console.log(total);
