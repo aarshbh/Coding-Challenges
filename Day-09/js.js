34c744b5e5f65d147691d0158e756c1b7c3d3db6
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-09 22:58:29 +0530

    Add files via upload

---
+++ Day-09/js.js
@@ -0,0 +1,24 @@
+ //    Today's task  //
+
+ /*
+ ###  Tasks  ###
+
+ 1. What will be the value of x in the following?
+ */
+
+ let x=30;
+ x+=10;
+ x/=2;
+
+ /* 2.create a variable called xp and initialize it to have a 
+ value of 250, multiple the value by 2 and console log the value.
+ */
+
+ //   <<  SOLUTION  >>   //
+
+ console.log("1. ",x);
+
+ let xp=250;
+ xp*=2;
+
+ console.log("2. ",xp);
