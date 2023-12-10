8fca638c3ce423470476146404c84e9d6889f19a
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-11 00:46:41 +0530

    Add files via upload

---
+++ Day-10/js.js
@@ -0,0 +1,19 @@
+// ###   TASKS   ### //
+/*
+1. Declare and initialize a variable with the value of
+100,000,log this to the console.
+
+2.In the console, identify the data type of the variable.
+
+3. Create a float data type, and log this type to the console.
+*/
+
+
+// <<<   SOLUTIONS   >>> //
+let num=1e5;
+console.log("1. ",num);
+
+console.log("2. ",typeof num);
+
+var numFloat = 3.14;
+console.log("3. ",typeof numFloat);
