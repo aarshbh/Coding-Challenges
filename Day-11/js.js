31de16c1c4326219bf0b56227ff3fb92552c63ad
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-12 02:41:47 +0530

    Add files via upload

---
+++ Day-11/js.js
@@ -0,0 +1,26 @@
+//   ### tasks ###   //
+/*
+1. Create three variables called s, d and b, and set their values to
+   the texts single, double and backtick, each one using the relevent 
+   type.
+
+2. Create a variable called speech and set it's value to, Martin Luther 
+   King said "I have a dream.Make sure to use the speech marks and
+   make sure there are no error.Console log speech.
+
+3. Create a variable called tip and set it's value
+    to We can escape quotes by using \.Console log tip.
+
+    */
+
+             //   <<< SOLUTIONS >>>   //    
+let s = 'Single';
+let d = "Double";
+let b = `Backtick`;
+console.log(s);
+console.log(d);
+console.log(b);
+let speech = `Martin Luther King said "i have Dream"`;
+console.log(speech);
+let tip = `"We can escape quotes by using \\."`
+console.log(tip);
