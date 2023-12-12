6768edaf74a02ef9f0bee43dbb0c7185f3c5c5c7
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-12 21:56:41 +0530

    Add files via upload

---
+++ Day-12/js.js
@@ -0,0 +1,21 @@
+//   ### TASKS ###   //
+/*
+
+1. Create two variables with the values " I am learning" and
+   "JavaScript" Concatinate these together using + operator and 
+    store the value in a new variable caller result. Console log result.
+
+2. Complete the same task , this time using the string concat().
+
+*/
+
+//   <<< SOLUTION >>>   //
+
+let a = "I am learning ";
+let b = "JavaScript";
+
+let result = a + b;
+console.log(result);
+
+let newResult= a.concat(b);
+console.log(newResult);
