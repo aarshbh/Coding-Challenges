eb06bc35e5d8c5e7f8b79681ad183e29e81c3a4c
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-13 22:18:03 +0530

    Add files via upload

---
+++ Day-13/js.js
@@ -0,0 +1,20 @@
+//   ### TASKS ###   //
+/*
+1. Create three variables , called firstname, courses, and channel, and 
+   assign the value your name to the name,JavaScript to the course and
+   Dev Dreamer to the channel.
+
+2. Create another variable called info , and use templet literals
+   to construnct the sentence , e.g. "John is Learning JavaScript
+   with Dev Dreamer.".
+   
+   */
+
+//   <<< SOLUTIONS >>>   //
+
+let firstName = `John`;
+let course = `JavaScript`;
+let channel = `Dev Dreamer`;
+let info = `${firstName} is learning ${course} with ${channel}`;
+console.log(info);
+
