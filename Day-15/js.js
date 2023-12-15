d44d1c25c61ebb77442654f491cc6f320fea602c
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2023-12-15 23:03:53 +0530

    Add files via upload

---
+++ Day-15/js.js
@@ -0,0 +1,28 @@
+//   ### TASKS ###   ///
+/*
+1. Create two variables called a and b. Assign the value 10 to a and
+   15 to b.Check to see if a is more than b, log  the answer to the 
+    console.
+
+2. Use the relevant comparison operator to identify whether a is equal
+   to "10". Log your answer to the console.
+   
+3. Check once again to see whether a is equal to "10", this time making 
+   sure to check against the type as well. Log your answer to the 
+    console.
+      
+4. If x is equal to "5" and y is equal to 6, what would 
+    the result of the following be ?
+    x !==y ?
+    
+  */
+
+//   <<< SOLUTIONS >>>   //
+let a=10;
+let b=15;
+console.log("1. "+ (a>b) );
+console.log("2. "+ (a ==  " 10 ") );
+console.log("3. "+ (a ===  " 10 ") );
+let x = "5";
+let y = 6;
+console.log("4. "+ (x!==y) );
